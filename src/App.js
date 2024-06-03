9e6688a7b1816295ff959d4c9d9d9e5010670d5f
Muskan <muskan@darukamuskan@gmail.com>
2024-06-03 14:51:54 +0530

    editor page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CodeEditor from "./components/CodeEditor";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Simple CodeEditor</h1>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/prism.js
@@ -0,0 +1,5 @@
+import Prism from 'prismjs';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css';
+
+export default Prism;
